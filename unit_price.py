f18823b6704f148c0847dd6e5d2435b9acb4b169
cheyennedq <69642300+cheyennedq@users.noreply.github.com>
2021-10-26 17:59:06 -0400

    Add files via upload

--- unit_price.py
+++ unit_price.py
@@ -4,23 +4,28 @@ import merge
 def calc_price(df, row_number, product_code, total_trade_dict):
     imp_exp_dict = {}
     for i in range(row_number):
-        print(df.iloc[i, 9])
         # temporarily only include World as a partner, year 2020
-        if df.iloc[i, 1] == 2020 & df.iloc[i, 12] == "World" & df.iloc[i, 21] == product_code:
+        if df.iloc[i, 1] == 2020 and df.iloc[i, 12] == "World" and df.iloc[i, 21] == product_code:
             # create dictionary of import & export values per country {'country': {M: $imp, X: $exp, ...]}
             # if partner is not already in the dictionary, add it
             if df.iloc[i, 9] not in imp_exp_dict:
                 # temporarily only include imports & exports
-                imp_exp_dict[df.iloc[i, 9]] = {"M": 0, "X": 0}
+                imp_exp_dict[df.iloc[i, 9]] = {'M': 0, 'X': 0}
             # calculation of average price: avg_price_1*prop(trade_value_1/total_trade)
             #   final result is a weighted sum: avg_price_1*prop(trade_value_1/total_trade)
             #   + avg_price_2*prop(trade_value_2/total_trade) + ... + avg_price_n*prop(trade_value_n/total trade)
             average_price = (df.iloc[i, 31] / df.iloc[i, 25]) * (
-                        df.iloc[i, 31] / total_trade_dict[df.iloc[i, 9]][product_code])
+                        df.iloc[i, 31] / total_trade_dict[df.iloc[i, 9]][product_code]['total'])
             # index by country, then by M or X depending on the trade flow
-            if df.iloc[i, 7].contains("M") | df.iloc[i, 7].contains("X"):
-                imp_exp_dict[df.iloc[i, 9]][df.iloc[i, 7]] += average_price
-            # price dict {'country': weighted_price} (return)
+            if "m" in df.iloc[i, 7].lower() and "r" not in df.iloc[i, 7].lower():
+                average_price = (df.iloc[i, 31] / df.iloc[i, 25]) * (df.iloc[i, 31] / total_trade_dict[df.iloc[i, 9]][product_code]['total_m'])
+                imp_exp_dict[df.iloc[i, 9]]['M'] += average_price
+            elif "x" in df.iloc[i, 7].lower() and "r" not in df.iloc[i, 7].lower():
+                average_price = (df.iloc[i, 31] / df.iloc[i, 25]) * (df.iloc[i, 31] / total_trade_dict[df.iloc[i, 9]][product_code]['total_x'])
+                imp_exp_dict[df.iloc[i, 9]]['X'] += average_price
+            # price dict {'country': {'M': weighted_price_imp, 'X': weighted_price_exp}} (return)
+    for key in imp_exp_dict.keys():
+        imp_exp_dict[key]['D'] = imp_exp_dict[key]['X'] - imp_exp_dict[key]['M']
     return imp_exp_dict
 
 
@@ -32,26 +37,28 @@ def total_trade(df, row_number):
     :return: dictionary of country-product-value triplets
     """
     total_trade_dict = {}
-    # {'country': {'product_1': tv_1, 'product_2': tv_2, ...}
+    # {'country': {'product_1': {'total': tv_1, 'M': num, 'X': num}, 'product_2': tv_2, ...}
     for i in range(row_number):
         # country not in dict
         if df.iloc[i, 9] not in total_trade_dict:
             total_trade_dict[df.iloc[i, 9]] = {}
         # product not in subdict
         if df.iloc[i, 21] not in total_trade_dict[df.iloc[i, 9]]:
-            total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]] = {'total': 0, 'M': 0, 'X': 0}
-        # add the trade value
+            total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]] = {'total': 0, 'total_m': 0, 'total_x': 0}
+        # add the total trade value
         total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]]['total'] += df.iloc[i, 31]
-        # increment M & X counter
-        if df.iloc[i, 7].contains("M") | df.iloc[i, 7].contains("X"):
-            total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]][df.loc[df.iloc[i, 7]]] += 1
+        # add the import/export value
+        if "m" in df.iloc[i, 7].lower() and "r" not in df.iloc[i, 7].lower():
+            total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]]['total_m'] += df.iloc[i, 31]
+        elif "x" in df.iloc[i, 7].lower() and "r" not in df.iloc[i, 7].lower():
+            total_trade_dict[df.iloc[i, 9]][df.iloc[i, 21]]['total_x'] += df.iloc[i, 31]
     return total_trade_dict
 
 
 # create dictionary of country-price pairs for histogram
 
 
-def price_histogram():
+def price_histogram(df, product):
     return
 
 
@@ -60,9 +67,13 @@ if __name__ == "__main__":
                          300220, 392620, 401511, 621010, 481850, 9020, 90200]
     # if in the dataframe, loop over otherwise skip
     merged_df = merge.merge_files()
+    print("merged files")
     rows = len(merged_df)
+    print("counted rows")
     tt_product = total_trade(merged_df, rows)
+    print("constructed total trade dictionary")
     print(tt_product)
     for product in product_code_list:
-        print(calc_price(merged_df, rows, product, tt_product))
+        product_dict = calc_price(merged_df, rows, product, tt_product)
         print(product)
+        print(product_dict)
